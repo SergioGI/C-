d1b29ce965f0cd52cce5f7348ed2477e26f0fe0d
SergioGI <63078859+SergioGI@users.noreply.github.com>
2022-08-03 20:56:10 -0500

    Add files via upload

---
+++ 3.-entrada de datos.cpp
@@ -0,0 +1,16 @@
+//lectura de entrada de datos
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	int numero;
+	
+	cout<<"digite un numero: ";
+	
+	cin>>numero;
+	
+	cout<<"el numero que digito es:"<<numero<<endl;
+	
+	return 0;
+}
