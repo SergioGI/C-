e0949be2f5af2177e17478e903e72ab50df906e6
SergioGI <63078859+SergioGI@users.noreply.github.com>
2022-08-03 20:55:52 -0500

    Add files via upload

---
+++ 2.-tipos de datos basicos en C++.cpp
@@ -0,0 +1,16 @@
+//tipos de datos basicos en C++
+
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	int numero=20;
+	float flotante=15.6;
+	double doble=25.165865;
+	char letra='S';
+	
+	cout<<flotante;
+	
+	return 0;
+}
